72d78c68a35e63c2390b7fa1755c3a7a47c26d53
Elipsce <simocurt9661@gmail.com>
2025-04-08 15:32:41 -0600

    Initial commit

---
+++ src/main/java/net/elipsce/nethercharcoal/NetherCharcoalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.elipsce.nethercharcoal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NetherCharcoalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
